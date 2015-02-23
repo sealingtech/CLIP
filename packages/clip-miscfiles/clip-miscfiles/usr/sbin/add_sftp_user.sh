7dd5d87192e5631e223c3ee02918f2bb571d18d0
Spencer Shimko <spencer@quarksecurity.com>
2015-02-23 14:50:58 -0500

    When adding sftp users, prompt for public keys.

--- packages/clip-miscfiles/clip-miscfiles/usr/sbin/add_sftp_user.sh
+++ packages/clip-miscfiles/clip-miscfiles/usr/sbin/add_sftp_user.sh
@@ -1,20 +1,8 @@
-#!/bin/sh
-# Copyright (C) 2014, Quark Security, Inc.
+#!/bin/bash
+# Copyright (C) 2014-2015, Quark Securityyyp, Inc.
 #
 # Aurhor: Brandon Whalen <brandon@quarksecurity.com>
-
-# I'm sure this will be better one day
-# but for now....
-# just pass in the new sftp user name and it'll do the tasks to
-# properly add a customer
-# semanage user -a -R "staff_r" $CUSTOMER_u
-# useradd -g sftp-only -Z $CUSTOMER_u $CUSTOMER
-# mkdir -m 710 /home/$CUSTOMER/.ssh
-# touch /home/$CUSTOMER/.ssh/authorized_keys
-# chown -R :sftp-only hypori/.ssh/
-# chmod 744 /home/$CUSTOMER/.ssh/authorized_keys
-# chcon -R  user_u:object_r:ssh_home_t:s0 /home/$CUSTOMER/.ssh/
-# PUT THE CUSTOMER KEY IN AUTHORIZED KEYS
+#	  Spencer Shimko <spencer@quarksecurity.com>
 
 add_user() {
         semanage user -a -R "staff_r" $1_u
@@ -23,9 +11,23 @@ add_user() {
         touch /home/$1/.ssh/authorized_keys
         chown -R :sftp-only /home/$1/.ssh/
         chmod 644 /home/$1/.ssh/authorized_keys
-        chcon -R  user_u:object_r:ssh_home_t:s0 /home/$1/.ssh/
         usermod -d /$1 $1
         usermod -s /sbin/nologin $1
+	semanage fcontext -a -s user_u -t ssh_home_t /home/$1/.ssh
+	semanage fcontext -a -s user_u -t ssh_home_t /home/$1/.ssh/authorized_keys
+	restorecon -RF /home/$1/.ssh
+}
+
+add_key() {
+	echo "Enter a public key for this user."
+	echo "You may quit or add more keys by pressing enter with a blank line."
+	while read -r key; do
+		[ -z "$key" ] && break
+		echo "$key" >> /home/$1/.ssh/authorized_keys""
+	done
+	read -r -p "Would you like to add another key (y/n)? " yes
+	[[ $yes == "y" ]] && add_key $1
+	restorecon -RF /home/$1/.ssh
 }
 
 if [ "$EUID" -ne 0 ]
@@ -33,7 +35,9 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 if [ "$#" -ne 1 ]; then
-        echo "Usage: add_customer.sh [customer]"
+        echo "Usage: $0 [customer]"
 else
         add_user $1
+	add_key $1
 fi
+
