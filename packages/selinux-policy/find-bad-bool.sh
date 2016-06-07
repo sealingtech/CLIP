d0c35e2d34a7b1b3e3f459a2fa71a1ef8a75a57a
Spencer Shimko <spencer@quarksecurity.com>
2016-06-07 17:36:57 -0400

    Fix path to fine bad bool gdb input

--- packages/selinux-policy/find-bad-bool.sh
+++ packages/selinux-policy/find-bad-bool.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd tmp/src/redhat/BUILD/selinux-policy
-gdb -x ../gdb_badbool --args semodule_expand tmp/test.lnk policy24
+gdb -x ../../../../../gdb_badbool --args semodule_expand tmp/test.lnk policy24
